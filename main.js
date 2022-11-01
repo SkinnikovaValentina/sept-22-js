aa4cbc9fd242ca30dbda1540e29140e836b35dcb
SkinnikovaValentina <ckrunnukova17@gmail.com>
2022-11-02 01:48:33 +0200

    JS-lesson1

---
+++ main.js
@@ -0,0 +1,45 @@
+let a = 'hello';
+let s = 'owu';
+let d = 'com';
+let f = 'ua';
+console.log(`${a} ${s} ${d} ${f}`)
+let g = 1;
+console.log(g)
+let h = 10;
+console.log(h)
+let j = -999;
+console.log(j)
+let k = 123;
+console.log(k)
+let l = 3.14;
+console.log(l)
+let z = 2.7;
+console.log(z)
+let x = 16;
+console.log(x)
+let c = 3 > 8;
+console.log(c)
+let v = 8 > 5;
+console.log(v)
+
+let firstName = 'Skrinnikova';
+let middleName = 'Valentina';
+let lastName = 'Borisovna';
+let person = firstName+ ' ' +middleName + ' ' +lastName
+console.log(person)
+
+
+let a1 = 100;
+console.log(typeof a1)
+let b1 = '100';
+console.log(typeof b1)
+let c1 = true;
+console.log(typeof c1)
+
+
+
+
+
+
+
+
