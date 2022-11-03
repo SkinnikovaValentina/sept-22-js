8f77e0c2b299b74aa54a822d0ac834ab03967c5c
SkinnikovaValentina <ckrunnukova17@gmail.com>
2022-11-03 15:02:57 +0200

    js-lesson2

---
+++ work2/main.js
@@ -0,0 +1,82 @@
+let arr = ['asd', 100, true, 'donna', 550, 'andrey', false, 200, 12, 'qwe'];
+console .log(arr[0])
+console .log(arr[1])
+console .log(arr[2])
+console .log(arr[3])
+console .log(arr[4])
+console .log(arr[5])
+console .log(arr[6])
+console .log(arr[7])
+console .log(arr[8])
+console .log(arr[9])
+
+let book1 = {
+    title: 'longWalk',
+    pageCount : 320,
+    genre : 'horror'
+}
+
+let book2 = {
+    title: 'shining' ,
+    pageCount : 370,
+    genre : 'horror'
+}
+
+let book3 = {
+    title: 'SteelRat',
+    pageCount : 650,
+    genre : 'science_fiction'
+}
+
+let book11 = {
+    title: 'long Walk',
+    pageCount : 320,
+    genre : 'horror',
+    author : [
+        name = 'Stephen_King',
+        age = 75
+    ]
+}
+
+let book21 = {
+    title: 'shining' ,
+    pageCount : 370,
+    genre : 'horror',
+    author : [
+        name = 'Stephen_King',
+        age = 75
+    ]
+}
+
+let book31 = {
+    title: 'stainless Steel Rat',
+    pageCount : 650,
+    genre : 'sci_fi',
+    author : [
+        name = 'Harry_Harrison',
+        age = 87
+    ]
+}
+
+let users = [
+    user1 = {name: 'poll', username:'poll1', password:111},
+    user2 = {name: 'mary', username:'mary2', password:222},
+    user3 = {name: 'katy', username:'katy3', password:333},
+    user4 = {name: 'bob', username:'bob4', password:444},
+    user5 = {name: 'nik', username:'nik5', password:555},
+    user6 = {name: 'lora', username:'lora6', password:666},
+    user7 = {name: 'ron', username:'ron7', password:777},
+    user8 = {name: 'rik', username:'rik8', password:888},
+    user9 = {name: 'yan', username:'yan9', password:999},
+     user10 = {name: 'donna', username:'donna0', password:123}
+]
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
